24bca601645bd4cfe59fff22184647a787e6f128
Amogh-007-Rin <amoghdath233@gmail.com>
2025-12-04 15:32:51 +0000

    Start

---
+++ OhMyFarm/src/app/MyFarm.java
@@ -0,0 +1,25 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MyFarm {
+	public static void main(String[] args) {
+		// Run me
+		displayTask();
+		
+	}
+	
+	public static void displayTask() {
+	    try (BufferedReader reader = new BufferedReader(new FileReader("task.txt"))) {
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            System.out.println(line);
+	        }
+	    } catch (IOException e) {
+	        System.out.println("Error reading file: " + e.getMessage());
+	    }
+	}
+
+}
